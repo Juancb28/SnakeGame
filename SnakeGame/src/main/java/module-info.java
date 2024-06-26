d0b9fe3138da9274cdfcad76aa4856805e2ed932
Juan Carlos Arroyo <112919837+Juancb28@users.noreply.github.com>
2024-06-25 23:28:07 -0500

    Delete SnakeGame directory

--- SnakeGame/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ec.edu.epn.snakegame {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens ec.edu.epn.snakegame to javafx.fxml;
-    exports ec.edu.epn.snakegame;
-}
