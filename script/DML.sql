ee031806d40618d982474dfca4dcebe751dca616
TheFer1 <estalyn2casa@gmail.com>
2024-08-04 12:11:55 -0500

    base de datos

---
+++ script/DML.sql
@@ -0,0 +1,5 @@
+-- database: ../database/data.sqlite
+INSERT INTO high_scores 
+ (player_name               ,score              ,survived_time  ) VALUES 
+ ('Player1'                 , 100               ,1000);
+ 
